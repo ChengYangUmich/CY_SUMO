28f5da736b1c56e1de10cb40acb7054708dd7c6f
Cheng Yang <yangche@umich.edu>
2022-03-07 16:13:54 -0500

    2022_03_07
    Write some documentations

---
+++ src/CY_SUMO.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from sumoscheduler import SumoScheduler
+from sumoscheduler import Duration as dur 
+import os
+import pandas as pd 
+import numpy as np
+import datetime
+
+print(dur.day)
